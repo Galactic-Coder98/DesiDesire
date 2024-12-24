a3461dfa2b95208af402f632446a3beea99444a9
Galactic-Coder98 <princechowdury32@gmail.com>
2024-12-24 02:43:13 -0800

    Changing homepage style and adding more designs

--- components/carousal.tsx
+++ components/carousal.tsx
@@ -8,6 +8,7 @@ const items: Item[] = [
   { id: 1, name: "Purple Velor Leghna’s", price: 649, seller: "Venus’ Vision", image_url: "https://mewinurmthovscbqwbbt.supabase.co/storage/v1/object/public/item-images/images/PurpleVelor.png" },
   { id: 2, name: "Rosè Red Sari’s 4 Rani’s", price: 899, seller: "Rani ka Sari’s", image_url: "https://mewinurmthovscbqwbbt.supabase.co/storage/v1/object/public/item-images/images/RedSari.png" },
   { id: 3, name: "Princess Pink Salwars", price: 1249, seller: "Gulabi Galore", image_url: "https://mewinurmthovscbqwbbt.supabase.co/storage/v1/object/public/item-images/images/GulabiPic.png" },
+  { id: 4, name: "Silver Green Elegance", price: 759, seller: "Venus’ Vision", image_url: "https://mewinurmthovscbqwbbt.supabase.co/storage/v1/object/public/item-images/images/GreenSari.png" },
 ];
 
 export default function Carousel(): JSX.Element {
