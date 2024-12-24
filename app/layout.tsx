ea0e9a995dbcb09db75a9384751016cc13783fb7
Galactic-Coder98 <princechowdury32@gmail.com>
2024-12-23 19:41:25 -0800

    Finishing up Navbar styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
               <main className="container mx-auto max-w-7xl px-6 flex-grow">
                 {children}
               </main>
-              <footer className="w-full flex items-center justify-center py-3">
+              <footer className="w-full flex items-center justify-center py-6">
                 <Link
                   className="flex items-center gap-1 text-current"
                   href="/"
